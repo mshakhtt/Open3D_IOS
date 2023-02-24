3567b52426b17970f4bc80f1cec7b5ead652f762
Mykola Shakh <mshah@tsukat.com>
2023-02-24 14:09:40 +0200

    update checksum

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import Open3D_iOSTests
-
-var tests = [XCTestCaseEntry]()
-tests += Open3D_iOSTests.allTests()
-XCTMain(tests)
--- Tests/Open3d_IOSTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(Open3d_iOSTests.allTests),
-    ]
-}
-#endif
